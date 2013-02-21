b840ac7d8339070e80531eb02dbad8231abea1ee
Marek Majkowski <majek04@gmail.com>
2013-02-21 21:18:52 +0000

    added some tests

--- multi_tictactoe.py
+++ multi_tictactoe.py
@@ -20,9 +20,20 @@ class Game():
         self.matrix[square] = player
 
     def legal_moves(self):
+        '''
+        >>> Game('x       x').legal_moves()
+        [1, 2, 3, 4, 5, 6, 7]
+        '''
         return [m for m in range(9) if self.matrix[m] == ' ']
 
     def winner_if_any(self):
+        '''
+        >>> Game('         ').winner_if_any()
+        >>> Game('xxx      ').winner_if_any()
+        'x'
+        >>> Game('a   a   a').winner_if_any()
+        'a'
+        '''
         WINCOMBOS = [[0,1,2], [3,4,5], [6,7,8],
                      [0,3,6], [1,4,7], [2,5,8],
                      [0,4,8], [2,4,6]]
@@ -46,10 +57,38 @@ class Game():
         return util[self.winner_if_any()]
 
     def is_over(self):
+        '''
+        >>> Game('x       x').is_over()
+        False
+        >>> Game('x   x   x').is_over()
+        'x'
+        >>> Game('abcdefghi').is_over()
+        True
+        '''
         empty_squares = sum(self.matrix[sq]== ' ' for sq in range(9))
         return self.winner_if_any() or not empty_squares
 
     def minimax(self, player='o', func=max):
+        '''
+        >>> Game('         ').minimax()
+        (0, 0)
+        >>> Game('oo       ').minimax()
+        (1, 2)
+        >>> Game('o  o     ').minimax() # is this a bug?
+        (1, 1)
+        >>> Game('o o      ').minimax()
+        (1, 1)
+        >>> Game('xx xx oo ').minimax()
+        (1, 8)
+        >>> Game('xx xx oo ').minimax('x', min) # bug?
+        (1, 8)
+        >>> Game('xx oo oo ').minimax('x', min)
+        (-1, 2)
+        >>> Game('xoxoxoox ').minimax('x', min)
+        (0, 8)
+        >>> Game('xoxoxoox ').minimax()
+        (0, 8)
+        '''
         board_key = "".join(self.matrix)
         if board_key in Game.memo:
             return Game.memo[board_key]
@@ -84,5 +123,7 @@ class Game():
 
 
 if __name__ == '__main__':
-    import pdb
-    pdb.set_trace()
+    # import pdb
+    # pdb.set_trace()
+    import doctest
+    doctest.testmod()
